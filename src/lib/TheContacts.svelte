4c92ca65b2060e5bc1baa786b57ef38f2d71eefe
Kantulaev <Kantulaev@gmail.com>
2023-02-08 13:52:08 +0600

    fix map

--- src/lib/TheContacts.svelte
+++ src/lib/TheContacts.svelte
@@ -1,11 +1,12 @@
 <h2 class="font-bold pt-8 text-center">Тлядал на карте</h2>
 <div style="position:relative;overflow:hidden" class="flex justify-center py-8">
   <iframe
-    title="Тлядал"
-    src="https://yandex.ru/map-widget/v1/?ll=46.239072%2C42.123669&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1MzEyMDU5OBJx0KDQvtGB0YHQuNGPLCDQoNC10YHQv9GD0LHQu9C40LrQsCDQlNCw0LPQtdGB0YLQsNC9LCDQptGD0L3RgtC40L3RgdC60LjQuSDRgNCw0LnQvtC9LCDQv9C%2B0YHQtdC70L7QuiDQotC70Y%2FQtNCw0LsiCg0s8zhCFbJ%2BKEI%3D&sctx=ZAAAAAgAEAAaKAoSCZ5eKcsQz0JAEdOgaB7A4EtAEhIJkiQIV0Bh8z8RcjWyKy0j2D8iBgABAgMEBSgKOABAAUgBYiFhZGRfc25pcHBldD10b3BvbnltX2Rpc2NvdmVyeS8xLnhiQnJlYXJyPXNjaGVtZV9Mb2NhbC9HZW8vTGlzdERpc2NvdmVyeS9FbmFibGVEaXNjb3ZlcnlUZXh0UmVxdWVzdHM9MWI1cmVhcnI9c2NoZW1lX0xvY2FsL0dlby9MaXN0RGlzY292ZXJ5L0VuYWJsZVJlcXVlc3RzPTFiOnJlYXJyPXNjaGVtZV9Mb2NhbC9HZW8vTGlzdERpc2NvdmVyeS9FbmFibGVFbXB0eVJlcXVlc3RzPTFiNXJlYXJyPXNjaGVtZV9Mb2NhbC9HZW8vTGlzdERpc2NvdmVyeS9FbmFibGVWZXJ0aWNhbD0xYjByZWFycj1zY2hlbWVfTG9jYWwvR2VvL0Fza0Rpc2NvdmVyeUZvclRvcG9ueW1zPTFiOnJlYXJyPXNjaGVtZV9Mb2NhbC9HZW8vTGlzdERpc2NvdmVyeS9FbmFibGVDb21tb25QaWNNZW51PTFqAnJ1nQHNzEw9oAEAqAEAvQHDUDKvwgECAADqAQDyAQD4AQCCAgzQotC70Y%2FQtNCw0LuKAgCSAgCaAgxkZXNrdG9wLW1hcHM%3D&sll=46.239072%2C42.123669&sspn=0.027709%2C0.011390&text=%D0%A2%D0%BB%D1%8F%D0%B4%D0%B0%D0%BB&z=15.45"
-    frameborder="1"
-    allowfullscreen="true"
+    title="Тлядал на карте"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23673.437305775897!2d46.23396470869141!3d42.125036863736405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4045fdb3aba120f7%3A0xdb75301b220156fd!2z0KLQu9GP0LTQsNC7LCDQoNC10YHQvy4g0JTQsNCz0LXRgdGC0LDQvSwg0KDQvtGB0YHQuNGPLCAzNjg0MTQ!5e0!3m2!1sru!2skg!4v1675842521356!5m2!1sru!2skg"
     class="w-full sm:w-[90%] h-[300px] sm:h-[500px]"
-    style="position:relative;"
+    style="border:0;"
+    allowfullscreen="true"
+    loading="lazy"
+    referrerpolicy="no-referrer-when-downgrade"
   />
 </div>
